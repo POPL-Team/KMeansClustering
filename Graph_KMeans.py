a0b61cd4ee10a99a177d33964104a2321df26ee0
An Le <46081192+Anle101@users.noreply.github.com>
2020-12-03 16:58:37 -0500

    Add files via upload

--- Graph_KMeans.py
+++ Graph_KMeans.py
@@ -1,7 +1,10 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
+#CSV file containing all centroid positions and their updates
 centroid_data = pd.read_csv('centroids.csv', delimiter=',')
+
+#Data points from our dataset
 data_points = pd.read_csv('lengthVsHeight.csv', delimiter = ',')
 
 print(centroid_data)
@@ -10,7 +13,6 @@ print(data_points)
 for i in range(0, len(centroid_data), 4):
     plt.scatter(data_points['length'], data_points['height_percentage'])
     
-    #plt.scatter(centroid_data.loc[i: i + 3]['x'], centroid_data.loc[i: i + 3]['y'])
     plt.scatter(centroid_data['x'][i], centroid_data['y'][i], c = "red")
     plt.scatter(centroid_data['x'][i + 1], centroid_data['y'][i + 1], c = "orange")
     plt.scatter(centroid_data['x'][i + 2], centroid_data['y'][i + 2], c = "lime")
